6446d5778922c9b86f2fe73a75520ad2eff34648
Marie Blakely <marieblakely@myyahoo.com>
2024-04-16 11:54:12 -0400

    embed ticketSchema

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,19 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0,
+  },
+}, {
+  timestamps: true
+})
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -25,6 +38,7 @@ const flightSchema = new Schema({
       return adjustedDate
     },
   }, 
+  tickets: [ticketSchema]
 }, {
   timestamps: true
 })
