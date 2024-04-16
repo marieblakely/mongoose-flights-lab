73a2ede3c145e923395f5c354883d9b7e89d9c2c
Marie Blakely <marieblakely@myyahoo.com>
2024-04-15 21:50:19 -0400

    create show route and controller function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,12 +1,58 @@
 import { Flight } from "../models/flight.js"
 
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights',
+    })
+  })
+  .catch(error => { 
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
 function newFlight (req, res) {
   res.render('flights/new', {
     title: 'Add Flight',
   })
 }
 
+function create (req, res) {
+  // for (let key in req.body) {
+	//   if (req.body[key] === '') delete req.body[key]
+	// }
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+function show(req, res) {
+  Flight.findById(req.params.flightId)
+  .then(flight => {
+    res.render('flights/show', { 
+      title: 'Flight Detail', 
+      flight: flight,
+    })    
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 
 export {
+  index,
+  create,
   newFlight as new,
+  show,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,12 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 // GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
 router.get('/new', flightsCtrl.new)
+
+router.get('/:flightId', flightsCtrl.show) 
+
+router.post('/', flightsCtrl.create)
 
 export { router }
